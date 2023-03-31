f11ed52fcb83adc998a8b27590d1dad8381e139d
HazelBCIT <ywang600@my.bcit.ca>
2023-03-31 16:01:40 -0700

    comment eventId

--- components/event-detail.js
+++ components/event-detail.js
@@ -5,14 +5,14 @@ function EventDetail(props) {
 
   return (
     <>
-      {/* <Image src="/event-samle.jpg" alt={title} width={250} height={160} />
+      <Image src="/event-samle.jpg" alt={title} width={250} height={160} />
       <h2>
         Event Name:
         {title}
       </h2>
       <time>Date: {date}</time>
       <address>Address: {address}</address>
-      <p>Content: {content}</p> */}
+      <p>Content: {content}</p>
     </>
   );
 }
