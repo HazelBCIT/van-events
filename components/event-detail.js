33084391a8537a466af20f588b3f58c339fe9ea7
HazelBCIT <ywang600@my.bcit.ca>
2023-03-31 14:38:04 -0700

    comment event detail

--- components/event-detail.js
+++ components/event-detail.js
@@ -1,18 +1,18 @@
 import Image from "next/image";
 
 function EventDetail(props) {
-  const { title, address, content, date} = props;
+  // const { title, address, content, date} = props;
 
   return (
     <>
       <Image src="/event-samle.jpg" alt={title} width={250} height={160} />
-      <h2>
+      {/* <h2>
         Event Name:
         {title}
       </h2>
       <time>Date: {date}</time>
       <address>Address: {address}</address>
-      <p>Content: {content}</p>
+      <p>Content: {content}</p> */}
     </>
   );
 }
