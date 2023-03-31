0e0c4dc7348bfd37d0be9564b31d6db51cfe34e8
HazelBCIT <ywang600@my.bcit.ca>
2023-03-31 14:09:52 -0700

    update no event 2

--- components/event-list.js
+++ components/event-list.js
@@ -5,13 +5,13 @@ function EventList(props) {
   
   return (
     <div style={{width:"100%", marginBottom:80}}>
-      {events?.map((event) => (
+      {events.map((event) => (
         <EventItem
-          key={event.id}
-          id={event.id}
-          title={event.title}
-          address={event.address}
-          date={event.date}
+          key={event?.id}
+          id={event?.id}
+          title={event?.title}
+          address={event?.address}
+          date={event?.date}
         />
       ))}
     </div>
